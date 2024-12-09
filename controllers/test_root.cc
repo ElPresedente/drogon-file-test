db5de7b6bd566d4848ffffc2816bf07bc356ef04
Воропаев Владимир Геннадьевич <vvoropaev@t-argos.ru>
2024-12-09 10:41:22 +0300

    отправка файла на клиент

--- controllers/test_root.cc
+++ controllers/test_root.cc
@@ -28,3 +28,23 @@ void test_root::file_handler(const HttpRequestPtr& req, std::function<void (cons
     auto resp = HttpResponse::newHttpJsonResponse(json);
     callback(resp);
 }
+
+void test_root::send_file(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, std::string p1) const{
+    std::string filePath = "./uploads/" + p1;
+    std::cout << filePath << std::endl;
+    if (!std::filesystem::exists(filePath)) {
+        // Возвращаем 404, если файл не найден
+        auto resp = drogon::HttpResponse::newNotFoundResponse();
+        resp->setBody("File not found");
+        callback(resp);
+        return;
+    }
+    auto resp = drogon::HttpResponse::newFileResponse(filePath);
+
+    // Устанавливаем заголовки
+    resp->setContentTypeString( "application/octet-stream" );
+    resp->addHeader("Content-Disposition", "attachment; filename=\"" + filePath + "\"");
+
+    // Отправляем ответ
+    callback(resp);
+}
--- controllers/test_root.h
+++ controllers/test_root.h
@@ -12,11 +12,14 @@ class test_root : public drogon::HttpController<test_root>
     // METHOD_ADD(test_root::get, "/{2}/{1}", Get); // path is /test_root/{arg2}/{arg1}
     // METHOD_ADD(test_root::your_method_name, "/{1}/{2}/list", Get); // path is /test_root/{arg1}/{arg2}/list
     // ADD_METHOD_TO(test_root::your_method_name, "/absolute/path/{1}/{2}/list", Get); // path is /absolute/path/{arg1}/{arg2}/list
-    ADD_METHOD_VIA_REGEX(test_root::handler,"/test_root/(.*)",Get);
-    ADD_METHOD_VIA_REGEX(test_root::file_handler,"/test_root/(.*)",Post);
+    ADD_METHOD_VIA_REGEX(test_root::handler,"/test_root/save/(.*)",Get);
+    ADD_METHOD_VIA_REGEX(test_root::file_handler,"/test_root/save/(.*)",Post);
+
+    ADD_METHOD_VIA_REGEX(test_root::send_file, "/test_root/get/(.*)", Get);
     METHOD_LIST_END
     // your declaration of processing function maybe like this:
     // void get(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, int p1, std::string p2);
     void handler(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, std::string p1) const;
     void file_handler(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback);
+    void send_file(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, std::string p1) const;
 };
